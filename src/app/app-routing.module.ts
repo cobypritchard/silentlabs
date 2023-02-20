1f88278f9afe91c05b3c425672f9bd8fa6f04552
Coby Pritchard <copritch@gmail.com>
2023-02-19 22:41:15 -0600

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aboutme', component :AboutmeComponent, pathMatch: 'full' },
+  { path: 'portfolio', component :PortfolioComponent, pathMatch: 'full' },
+  { path: 'blog', component :BlogComponent, pathMatch: 'full' },
+  { path: 'contact', component :ContactComponent, pathMatch: 'full' },
+  { path: 'resume', component :ResumeComponent, pathMatch: 'full' },
+  { path: 'services', component :ServicesComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ServicesComponent } from './services/services.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    ResumeComponent,
+    ServicesComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
